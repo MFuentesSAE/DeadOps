d344e804c698f6fdac0b6ac2f61687c792d4d780
AntonioEM99 <antonio.espinosa@saei.mx>
2026-01-22 00:21:20 -0600

    Inicial

---
+++ Source/DeadOps/DeadOps.cpp
@@ -0,0 +1,9 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "DeadOps.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, DeadOps, "DeadOps" );
+
+DEFINE_LOG_CATEGORY(LogDeadOps)
+ 
